7d0b452bc4098b0c6d3d3c1b1ada930a7b6c8566
Al-ImranRony <imranrony687@gmail.com>
2020-07-07 20:52:55 +0600

    Run time & Complexity based - Primes

---
+++ hrRunTimeComplexity.py
@@ -0,0 +1,34 @@
+import sys
+import math
+
+sys.stdin = open("input.txt", "r")
+sys.stdout = open("output.txt", "w")
+
+
+def CheckPrime(n, c):
+    # It checks the primality in the worst case scenario
+    if n == 1 or n % 2 == 0:
+        c += 1
+        print("Not prime", "-> Iterration needed:", c)
+        return
+    if n == 2:
+        c += 1
+        print("Prime", "-> Iterration needed:", c)
+        return
+
+    sr = int(math.sqrt(n))
+
+    for i in range(3, sr + 1, 2):
+        c += 1
+        if n % i == 0:
+            print("Not prime", "-> Iterration needed:", c)
+
+    print("Prime", "-> Iterration needed:", c)
+
+
+for _ in range(int(input())):
+    n = int(input())
+    c = 0
+    CheckPrime(n, c)
+
+    
---
+++ spojPrimGenerator.py
@@ -0,0 +1,21 @@
+import sys
+from math import sqrt
+
+sys.stdin = open("input.txt", "r")
+sys.stdout = open("output.txt", "w")
+
+# By brute force
+
+for _ in range(int(input())):
+    m, n = map(int, input().split())
+    primes = {}
+    for i in range(2, int(sqrt(n)+1)):
+        for j in range(max(m//i, 2), (n//i)+1):
+            primes[i * j] = 1
+
+    for i in range(max(m, 2), n+1):
+        if i not in primes:
+            print(i) 
+
+    
+
