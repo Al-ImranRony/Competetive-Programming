16f721c14f3416dd69aaf49808f0c8202adf0a50
Al-ImranRony <imranrony687@gmail.com>
2020-08-26 14:36:49 +0600

    Default template for competitive programming

---
+++ template.py
@@ -0,0 +1,20 @@
+# My template for CP
+
+import sys
+import math
+import heapq        # helps to return the top n smallest/largest elems
+import collections
+from collections import Counter, defaultdict
+     
+
+class Solution:
+    def func(self, x, n):
+        return x
+
+if __name__ == '__main__': 
+    obj = Solution()
+    result = obj.func([1,2,3,4,5,6,7,8,9], [2,3,6])
+    print(result)
+
+zipped = zip(dict.values(), dict.keys())
+print(sorted(zipped))                             # sorted by values 
