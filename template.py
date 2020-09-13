7fc9aaa6f77af24684cb50e0c1fbe981c70d4185
Al-ImranRony <imranrony687@gmail.com>
2020-09-13 23:47:44 +0600

    M: Template for py coding

--- template.py
+++ template.py
@@ -2,24 +2,27 @@
 
 import sys
 import math
-import heapq        # helps to return the top n smallest/largest elems
+import heapq 
 import random
 import collections
 from collections import Counter, defaultdict
+from itertools import combinations, permutations
 
-     
+
 for _ in range(int(input())):
     x, y, k, n = map(int, input().split())
 
-    
+
 class Solution:
     def func(self, x, n):
         return x
 
-if __name__ == '__main__': 
+
+if __name__ == "__main__":
     obj = Solution()
-    result = obj.func([1,2,3,4,5,6,7,8,9], [2,3,6])
+    result = obj.func([1, 2, 3, 4, 5, 6, 7, 8, 9], [2, 3, 6])
     print(result)
 
 zipped = zip(dict.values(), dict.keys())
-print(sorted(zipped))                             # sorted by values 
+print(sorted(zipped))  # sorted by values
+
