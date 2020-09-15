d336fe75a6c45f0760ce58a5c231707757460f14
Al-ImranRony <imranrony687@gmail.com>
2020-09-15 23:15:40 +0600

    LeetCode Sept challenge day-15; another way using rstrip() in words.

--- lc-lenOfLastWord.py
+++ lc-lenOfLastWord.py
@@ -1,7 +1,7 @@
 # LeetCode Sept challenge 15
 
 def lengthOfLastWord(s: str) -> int:     
-    words = s.split()  
+    words = s.split()      
     if words: return len(words[-1])
     return 0
 
@@ -9,3 +9,8 @@ def lengthOfLastWord(s: str) -> int:
 print(lengthOfLastWord("Hello World"))
 print(lengthOfLastWord(" "))
 print(lengthOfLastWord("a "))
+
+# Another Solution can be ...
+'''
+len(s.rstrip(' ').split(' ')[-1])
+'''
