861a1fd21d126169da42b9f13a56fb19216d9d8a
Al-ImranRony <imranrony687@gmail.com>
2020-10-02 00:02:04 +0600

    LeetCode September challenge day-30

--- lc-FirstMissingPositive.py
+++ lc-FirstMissingPositive.py
@@ -2,10 +2,10 @@
 
 def firstMissingPositive(nums) -> int:
     for i in range(len(nums)):
-        if 0 <= nums[i]-1 < len(nums) and (nums[nums[i]-1] != nums[i]):
-            temp = nums[i] - 1
-            nums[i], nums[temp] = nums[temp], nums[i]
-
+            while 0 <= nums[i]-1 < len(nums) and (nums[nums[i]-1] != nums[i]):
+                tmp = nums[i]-1
+                nums[i], nums[tmp] = nums[tmp], nums[i]
+                
     for i in range(len(nums)):
         if nums[i] != i+1:
             return i+1
