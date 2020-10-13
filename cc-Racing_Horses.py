dc72de1f3f2f55592198f41316eca14a1ff94182
Al-ImranRony <imranrony687@gmail.com>
2020-10-14 01:05:51 +0600

    Codechef sorting problem

--- cc-Racing_Horses.py
+++ cc-Racing_Horses.py
@@ -1,7 +1,6 @@
 
-
-for _ in range(int(input())):
+for i in range(int(input())):
     n = int(input())
     a = list(map(int, input().split()))
-    a = sorted(a)
-    print(a[1]-a[0])
+    a.sort()
+    print(min(abs(a[i]-a[i+1]) for i in range(0, n-1)))
