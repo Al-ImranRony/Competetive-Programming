18a73098f12b289538738ffbfcb064218bba3422
Al-ImranRony <imranrony687@gmail.com>
2020-10-17 23:23:32 +0600

    Codechef practice

--- Count_Substrings.py
+++ Count_Substrings.py
@@ -1,6 +1,16 @@
 # Codechef easy practice
 
-for i in range(int(input())):
+import sys
+import math
+import heapq 
+import random
+import collections
+from collections import Counter, defaultdict
+from itertools import combinations, permutations
+
+
+for _ in range(int(input())):
     n = int(input())
     s = input()
-    #find substrings first
+    ones = s.count('1')
+    print(ones + (ones*(ones-1)//2))
