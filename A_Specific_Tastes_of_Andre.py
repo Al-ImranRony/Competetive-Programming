2e4aeb247754c987003ac89ee082abf1aefe7683
Al-ImranRony <imranrony687@gmail.com>
2020-11-13 23:48:16 +0600

    Codeforces round 682, div2-a

---
+++ A_Specific_Tastes_of_Andre.py
@@ -0,0 +1,15 @@
+# Codeforces round 682, div2-a
+
+import sys
+import math
+import heapq 
+import random
+import collections
+from collections import Counter, defaultdict
+from itertools import combinations, permutations
+
+
+for _ in range(int(input())):
+    x, y, k, n = map(int, input().split())
+
+    arr = list(map(int, input().split()))
