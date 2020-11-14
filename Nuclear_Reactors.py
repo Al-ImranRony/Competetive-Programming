f4df5dfd1db7fd21a68a97d3c50522bbea3baa7f
Al-ImranRony <imranrony687@gmail.com>
2020-11-14 23:58:50 +0600

    Codechef easy problem

---
+++ Nuclear_Reactors.py
@@ -0,0 +1,13 @@
+# Codechef easy problem 
+
+import sys
+import math
+import heapq 
+import random
+import collections
+from collections import Counter, defaultdict
+from itertools import combinations, permutations
+
+
+for _ in range(int(input())):
+    x, y, k, n = map(int, input().split())
