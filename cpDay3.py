929fbf039e4a7d8e4e2c106079102743647bf618
Al-ImranRony <imranrony687@gmail.com>
2020-11-17 00:27:28 +0600

    Daily coding challenge (google's interview)

--- cpDay3.py
+++ cpDay3.py
@@ -1,9 +1,16 @@
 # Day 3 of 100daysCodingChallenge
 # Day 1 of daily coding challenge
 
-arr, k = [10, 15, 3, 7], 17
-for i in range(len(arr)):
-    if (k - arr[i]) in arr:
-        print("True")
-        break
-print("False")
+
+def k_pair(arr, k):
+    for i in range(len(arr)):
+        if (k - arr[i]) in arr:
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    arr = list(map(int, input("Enter an array: ").split()))
+    k = int(input("Enter k: "))
+    ans = k_pair(arr, k)
+    print(ans)
