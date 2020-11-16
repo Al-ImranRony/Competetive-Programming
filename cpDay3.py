cbef7a3d39e026149163ac0847c39f7e243fbf82
Al-ImranRony <imranrony687@gmail.com>
2020-11-16 23:59:41 +0600

    Day 1 of daily coding challenge

--- cpDay3.py
+++ cpDay3.py
@@ -1,5 +1,9 @@
 # Day 3 of 100daysCodingChallenge
+# Day 1 of daily coding challenge
 
 arr, k = [10, 15, 3, 7], 17
-for i in range(arr):
-    
+for i in range(len(arr)):
+    if (k - arr[i]) in arr:
+        print("True")
+        break
+print("False")
