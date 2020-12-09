bc95d4cf3bbef705cb5788a13286433f0d331065
Al-ImranRony <imranrony687@gmail.com>
2020-12-10 01:29:50 +0600

    Codechef Dec long challenge 3

--- Even_Pair_Sum.py
+++ Even_Pair_Sum.py
@@ -11,8 +11,11 @@ from itertools import combinations, permutations
 for _ in range(int(input())):
     a, b = map(int, input().split())
     evPair, i, j = 0, 1, 1
-    while (i<=a and j<=b):
-        if (i + j)%2 == 0: evPair += 1
-        print(i, j)
-        i, j = i+1, j+1
+    for i in range(1, a+1):
+        if (i%2) == 0: 
+            for j in range(2, b+1, 2):
+                if ((i+j)%2) == 0: evPair += 1
+        else:
+            for j in range(1, b+1, 2):
+                if ((i+j)%2) == 0: evPair += 1
     print(evPair)
