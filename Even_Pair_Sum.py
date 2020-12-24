632291fa0aa27274f9583e5b18add1ceb772aaff
Al-ImranRony <imranrony687@gmail.com>
2020-12-24 23:59:23 +0600

    Codechef Dec Challenge

--- Even_Pair_Sum.py
+++ Even_Pair_Sum.py
@@ -10,12 +10,8 @@ from itertools import combinations, permutations
 
 for _ in range(int(input())):
     a, b = map(int, input().split())
-    evPair, i, j = 0, 1, 1
-    for i in range(1, a+1):
-        if (i%2) == 0: 
-            for j in range(2, b+1, 2):
-                if ((i+j)%2) == 0: evPair += 1
-        else:
-            for j in range(1, b+1, 2):
-                if ((i+j)%2) == 0: evPair += 1
-    print(evPair)
+    aEven = a//2
+    aOdd= a-aEven
+    bEven = b//2
+    bOdd = b-bEven
+    print(aEven*bEven + aOdd*bOdd)
