7e01c99f20deec6647906daf6a498c1f02d8558d
Al-ImranRony <imranrony687@gmail.com>
2020-12-28 00:12:00 +0600

    stack-queue solution

--- lc-StudentsUnableToEatLunch.py
+++ lc-StudentsUnableToEatLunch.py
@@ -6,19 +6,31 @@ from itertools import product
 
 
 class Solution:
-    def countStudents(self, students, sandwiches) -> int:
-        gols, borgos = 0, 0
-        for stu in students:
-            if (stu == 0): gols += 1
-            else: borgos += 1
-        
-        for i in range(len(sandwiches)):
-            if (sandwiches[i] == 0): gols -= 1
-            else: borgos -= 1
-        
-            if (gols < 0 or borgos < 0): return len(students)-i
+    def countStudents(self, students, sandwiches):
+        students = deque(students)
+        for sw in sandwiches:
+            if sw in students:
+                while (students[0] != sw):
+                    students.append(students.popleft())
+                students.popleft()
+            else:
+                return len(students)
         
         return 0
+
+    # def countStudents(self, students, sandwiches) -> int:
+    #     gols, borgos = 0, 0
+    #     for stu in students:
+    #         if (stu == 0): gols += 1
+    #         else: borgos += 1
+        
+    #     for i in range(len(sandwiches)):
+    #         if (sandwiches[i] == 0): gols -= 1
+    #         else: borgos -= 1
+        
+    #         if (gols < 0 or borgos < 0): return len(students)-i
+        
+    #     return 0
 
 if __name__ == '__main__': 
     obj = Solution()
