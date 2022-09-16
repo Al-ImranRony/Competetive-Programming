dbd72e77f395991f06382a440e061bd1c76397e4
Al-ImranRony <imranrony687@gmail.com>
2022-09-17 00:01:15 +0600

    Substring matching approach

--- lc-PermutaionInString.py
+++ lc-PermutaionInString.py
@@ -2,39 +2,18 @@
 First intuition solution, need too much time  O(n * log(n) * n), obvious TLE !
 '''
 
+from collections import Counter
 
 def checkInclusion(s1, s2):
-    res = False
-    permutationS = []
-    p = permute(s1, s2, "", permutationS)
-    for each in p:
-        if (each not in s2):
-            res = False
-        else:
-            res = True
-            break
+    ln1, ln2 = len(s1), len(s2)
+    s1CharFreq = Counter(s1)
+
+    for i in range(ln2):
+        window = s2[i: i+ln1]
+        s2WindowFreq = Counter(window)
+
+        if (s1CharFreq == s2WindowFreq):
+            return True
+        
+    return False
     
-    return res
-
-
-def permute(s1, s2, ans, permutationS):
-    ln = len(s1)
-    alpha = [False] * 26
-    
-    if (ln == 0):
-        permutationS.append(ans)
-        print(ans, permutationS)
-
-    for i in range(ln):
-        char = s1[i]
-        left, right = s1[0:i], s1[i+1: ]
-        rest = left + right
-
-        if (alpha[ord(char) - ord('a')] == False):
-            permute(rest, s2, ans+char, permutationS)
-        alpha[ord(char) - ord('a')] = True
-    
-    return permutationS
-
-
-print(checkInclusion("aabc", "adkjhbaacwruy"))
